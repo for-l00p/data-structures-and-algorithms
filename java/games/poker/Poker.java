e540b1a0a007bc50e71794a8c556e387711a7288
Piyush Ahuja <piyushahuja.in@gmail.com>
2019-02-10 14:43:22 +0530

    removed nested classes from Poker

--- java/games/poker/Poker.java
+++ java/games/poker/Poker.java
@@ -10,13 +10,10 @@ import java.util.*;
 import java.lang.reflect.Field;
 
 class Poker {
-//Root class which has all the classes needed.
-	List<Player> players;
-	int minimumBet;
 
 	public Poker (int numPlayers, int numRounds){
-		players = GameEngine.initPlayers(numPlayers);
-		minimumBet = GameEngine.initMinimumBet();
+		List<Player> players = GameEngine.initPlayers(numPlayers);
+		int minimumBet = GameEngine.initMinimumBet();
 		for (int i = 0; i < numRounds; i ++ ){
 			int smallBlindIndex = i % players.size();
 			Game game = new Game(players, smallBlindIndex , minimumBet);
